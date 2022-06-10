a9850231d20cb2df77201f398e46a0b710ceda17
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-10 16:23:41 +0530

    2.Perfect Number

--- logicalProgram/logicalProgram/Program.cs
+++ logicalProgram/logicalProgram/Program.cs
@@ -7,8 +7,17 @@ namespace logicalProgram
     {
         public static void Main(string[] args)
         {
-            Fibonacci fib = new Fibonacci();
-            fib.fibonacci();
+            //Fibonacci fib = new Fibonacci();
+            //fib.fibonacci();
+
+            perfectNumber num = new perfectNumber();
+            num.perfectnum();
+
+            //primeNumber num = new primeNumber();
+            //num.primenumber();
+
+            //reverseNumber number = new reverseNumber();
+            //number.Reverse_number();
         }
     }   
 }
