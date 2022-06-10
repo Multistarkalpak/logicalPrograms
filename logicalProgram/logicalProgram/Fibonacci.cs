1f08134990e6a1b813f4140d477af48c918dca60
Multistarkalpak <105732959+Multistarkalpak@users.noreply.github.com>
2022-06-10 15:56:35 +0530

    Fibonacci series

---
+++ logicalProgram/logicalProgram/Fibonacci.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicalProgram
+{
+    internal class Fibonacci
+    {
+            
+        public int num1 = 0;
+        public int num2 = 1;
+
+        public void fibonacci()
+        {
+            int n = 8;
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+
+            for (int i = 1; i <=n; i++)
+            {
+                int newNum = num1 + num2;
+
+                Console.WriteLine(newNum);
+
+                num1 = num2;
+                num2 = newNum;
+
+            }
+        }
+    }
+}
---
+++ logicalProgram/logicalProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+namespace logicalProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Fibonacci fib = new Fibonacci();
+            fib.fibonacci();
+        }
+    }   
+}
